f1a2b98918fd0dca5c22931efba18cccb0baebe3
dohatir <dohagit0@gmail.com>
2025-01-14 00:28:50 +0100

    commit eureka

--- Eureka/Client/src/main/java/exemple/eurekaclient/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package exemple.eurekaclient.repository;
-
-import exemple.eurekaclient.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- Eureka/ClientFeign/src/main/java/exemple/eurekafeignclient/repository/VoitureRepository.java
+++
@@ -1,9 +0,0 @@
-package exemple.eurekafeignclient.repository;
-
-import exemple.eurekafeignclient.entities.Voiture;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoitureRepository extends JpaRepository<Voiture, Long> {
-}
